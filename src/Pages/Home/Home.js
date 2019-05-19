c7a208984904350387ac455c664f2d41dbae1f59
mc0918 <mcampbell0918@gmail.com>
2019-05-19 18:22:39 -0500

    started working on components and pages

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import Header from "../../Components/Header/Header";
+
+function Home() {
+  return (
+    <div className="container">
+      <Header />
+      <div>Welcome!</div>
+    </div>
+  );
+}
+
+export default Home;
