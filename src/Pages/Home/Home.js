9e6c8bbeaefbf51de0b115cdff0087adfe973944
mc0918 <mcampbell0918@gmail.com>
2019-05-27 15:04:25 -0500

    added materialize sticky footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./style.css";
 import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
 
 function Home() {
   return (
-    <div className="content-box">
-      <Header />
-      <div>Welcome!</div>
+    <div className="pageWrapper">
+      <div className="content-box">
+        <Header />
+        <div>Welcome!</div>
+      </div>
+      <Footer />
     </div>
   );
 }
