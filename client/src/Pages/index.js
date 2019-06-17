87c2d2390756aa54a5524403c4a591300a99dab8
mc0918 <mcampbell0918@gmail.com>
2019-06-17 15:24:28 -0500

    next.js demo working, type 'npm run next' to see it

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
-
+import Header from "../Components/Header/Header";
 export default () => (
   <div>
-    hi
+    <Header />
     <Link href="/symba">
       <a>Symba</a>
     </Link>
